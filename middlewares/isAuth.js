f4cbd95d4d90ad70b963fb0063c2a5166ae5d4cc
Ritesh Rai <riteshrai997@gmail.com>
2021-09-04 20:59:32 +0530

    token limit counter and dailyLimitExpiration date

--- middlewares/isAuth.js
+++ middlewares/isAuth.js
@@ -14,7 +14,32 @@ const isAuth = (req, res, next) => {
         message: "Invalid token",
       });
     }
-    //if token is valid
+    // check if token.dailyLimitExpiration is less than current time then make token.dailyLimit = 5
+    let check = false;
+    if (token.dailyLimitExpiration < Date.now()) {
+      token.dailyLimit = 5;
+      token.dailyLimitExpiration = Date.now() + 86400000;
+      check = true;
+      async function saveToken() {
+        await token.save();
+      }
+      saveToken();
+    }
+    // check if token.dailyLimit is less than 0 then return error
+    if (token.dailyLimit <= 0) {
+      return res.status(401).send({
+        message: "Daily limit exceeded",
+      });
+    }
+    // check if token.dailyLimit is greater than 0 then decrement token.dailyLimit by 1
+    if (token.dailyLimit > 0 && check == false) {
+      token.dailyLimit--;
+      async function saveToken() {
+        await token.save();
+      }
+      saveToken();
+    }
+
     next();
   });
 };
--- models/tokenModel.js
+++ models/tokenModel.js
@@ -18,6 +18,17 @@ const tokenSchema = new mongoose.Schema(
       // set default value to current time + 1 Month
       default: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
     },
+    dailyLimit: {
+      type: Number,
+      required: true,
+      default: 5,
+    },
+    dailyLimitExpiration: {
+      type: Date,
+      required: true,
+      // set default value to current time + 1 Day
+      default: new Date(Date.now() + 1000 * 60 * 60 * 24),
+    },
   },
   {
     timestamps: true,
